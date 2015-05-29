8bca808678a9a2c9de2bae2dc4bc3a283962a8c5
Jeremy Kersten <jke@odoo.com>
2015-05-29 17:13:02 +0200

    add field on sale order

--- ecom_yenth/controllers.py
+++ ecom_yenth/controllers.py
@@ -6,11 +6,23 @@ from openerp.addons.website_sale.controllers.main import website_sale
 
 class website_yenth(website_sale):
 
+    @http.route(['/shop/extra_add_color'], type='http', auth="public", website=True)
+    def add_color(self, **post):
+        if post.get('color'):
+            color = post.get('color')
+            cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
+            pool['sale.colorpicker'].create(cr, uid, {'name': color, 'website_publish': True}, context=context)
+            return request.make_response("<h1> Color : %s added ...</h1>" % color)
+        return request.make_response("<h1> Color param missing...</h1>")
+
     @http.route(['/shop/extra'], type='http', auth="public", website=True)
     def extra(self, **post):
+        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         order = request.website.sale_get_order()
 
         if post:
+            if post.get('color'):
+                order.write({'color': int(post.get('color'))})
             for line in order.order_line:
                 line.write({
                     'isGift': bool(post.get('%s-%s' % (line.id, 'gift'))),
@@ -18,8 +30,9 @@ class website_yenth(website_sale):
                 })
             request.session['extra_info_done'] = True
             return request.redirect("/shop/checkout")
-
-        values = dict(order=order)
+        color_ids = pool['sale.colorpicker'].search(cr, uid, [('website_publish', '=', True)], context=context)
+        colors = pool['sale.colorpicker'].browse(cr, uid, color_ids, context=context)
+        values = dict(order=order, colors=colors)
         return request.website.render("ecom_yenth.extra", values)
 
     @http.route(['/shop/confirm_order'], type='http', auth="public", website=True)
--- ecom_yenth/models.py
+++ ecom_yenth/models.py
@@ -7,3 +7,16 @@ class sample(models.Model):
     _inherit = 'sale.order.line'
     numberOfUnits = fields.Integer()
     isGift = fields.Boolean()
+
+
+# TODO: access rule to do...
+class sale_colorpicker(models.Model):
+    _name = 'sale.colorpicker'
+    _description = 'dropdown for order/quotation (pick colors)'
+    name = fields.Char('Name', required=True)
+    website_publish = fields.Boolean('Published')
+
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+    color = fields.Many2one('sale.colorpicker', 'Color')
